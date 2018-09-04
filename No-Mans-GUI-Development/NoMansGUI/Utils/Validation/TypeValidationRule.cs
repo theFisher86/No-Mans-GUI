6d427066235e5dd818e3f3a3c4bd33d8d09b9359
wannabeuk <xfxmaximus@hotmail.com>
2018-09-04 22:15:07 +0100

    Begun Validation Work.

---
+++ No-Mans-GUI-Development/NoMansGUI/Utils/Validation/TypeValidationRule.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace NoMansGUI.Utils.Validation
+{
+    public class TypeValidationRule : ValidationRule
+    {
+        public Type Type
+        {
+            get;
+            set;
+        }
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (!(value is Type))
+            {
+                return new ValidationResult(false, "Illegal Type");
+            }
+            else
+            {
+                return ValidationResult.ValidResult;
+            }
+        }
+    }
+}

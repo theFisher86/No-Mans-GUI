accd513265659e31a4caae21795d5c3f49d1b65f
Ben Murray <xfxmaximus@hotmail.com>
2018-09-19 15:43:11 +0100

    Started Working on WorkSpace

---
+++ No-Mans-GUI-Development/NoMansGUI/ViewModels/BaseVMs/DocumentBase.cs
@@ -0,0 +1,90 @@
+﻿using Caliburn.Micro;
+using System;
+
+namespace NoMansGUI.ViewModels
+{
+    public abstract class DocumentBase : Screen
+    {
+        private bool _isForcedClosing;
+        private string _id;
+
+        /// <summary>
+        /// Gets or sets the document content identifier. This might be a file path,
+        /// a database PK, etc.
+        /// </summary>
+        public string ID
+        {
+            get { return _id; }
+            set
+            {
+                if(value != _id)
+                {
+                    _id = value;
+                    NotifyOfPropertyChange(() => ID);
+                }
+            }
+        }
+
+        public abstract bool IsDirty();
+
+        public override void CanClose(Action<bool> callback)
+        {
+            // if forcing (@@hack) or not dirty it's OK to close
+            if ((_isForcedClosing) || (!IsDirty()))
+            {
+                callback(true);
+                return;
+            }
+
+            // else prompt user
+            MessageBoxAction prompt = new MessageBoxAction
+            {
+                Caption = App.APP_NAME,
+                Text = LocalizedStrings.Format(StringResources.DiscardDocumentChangesPrompt,
+                    DisplayName),
+                Button = MessageBoxButton.YesNo,
+                Image = MessageBoxImage.Question
+            };
+            prompt.Completed += (sender, e) =>
+            {
+                callback(prompt.Result == MessageBoxResult.Yes);
+            };
+            prompt.Execute(null);
+        }
+
+        public bool CanClose()
+        {
+            if (!IsDirty()) return true;
+
+            MessageBoxAction prompt = new MessageBoxAction
+            {
+                Caption = App.APP_NAME,
+                Text = LocalizedStrings.Format(StringResources.DiscardDocumentChangesPrompt,
+                    DisplayName),
+                Button = MessageBoxButton.YesNo,
+                Image = MessageBoxImage.Question
+            };
+
+            bool bResult = true;
+            prompt.Completed += (sender, e) =>
+            {
+                bResult = prompt.Result == MessageBoxResult.Yes;
+            };
+            prompt.Execute(null);
+            return bResult;
+        }
+
+        public void Close()
+        {
+            _isForcedClosing = true;
+            try
+            {
+                TryClose();
+            }
+            finally
+            {
+                _isForcedClosing = false;
+            }
+        }
+    }
+}
---
+++ No-Mans-GUI-Development/NoMansGUI/ViewModels/WorkspaceViewModel.cs
@@ -0,0 +1,63 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace NoMansGUI.ViewModels
+{
+    public class WorkspaceViewModel : Conductor<IScreen>.Collection.OneActive, IShell
+    {
+        private readonly IEventAggregator _eventAggregator;
+        private readonly IWindowManager _windowManager;
+
+        public BindableCollection<ToolBase> Tools { get; private set; }
+
+        public BindableCollection<DocumentBase> Documents { get; private set; }
+
+        public WorkspaceViewModel(IEventAggregator eventAggregator, IWindowManager windowManager, [ImportMany]IEnumerable<ToolBase> tools)
+        {
+            if (tools == null)
+            {
+                throw new ArgumentNullException("tools");
+            }
+
+            _eventAggregator = eventAggregator ?? throw new ArgumentNullException("eventAggregator");
+            _windowManager = windowManager ?? throw new ArgumentNullException("windowManager");
+
+            Tools = new BindableCollection<ToolBase>(tools);
+            Documents = new BindableCollection<DocumentBase>();
+
+            Items.CollectionChanged += (sender, args) =>
+            {
+                if (args.Action == NotifyCollectionChangedAction.Remove)
+                {
+                    foreach (var item in args.OldItems.OfType<DocumentBase>())
+                    {
+                        Documents.Remove(item);
+                    }
+                }
+            };
+
+            _eventAggregator.Subscribe(this);
+        }
+
+
+        public void AddDocument(string id)
+        {
+            var documents = Items.OfType<DocumentBase>();
+            DocumentBase document = documents.FirstOrDefault(e => e.ID == id);
+            if(document != null)
+            {
+                ActivateItem(document);
+                return;
+            }
+
+            
+        }
+    }
+}
